f15a8afb8a1422d3d0d8e1a4bccf207200e37305
hasan <mdhasanhk.98@gmail.com>
2022-08-03 03:47:30 -0400

    country

--- app.js
+++ app.js
@@ -1,19 +1,27 @@
-
 fetch('https://restcountries.com/v3.1/all')
 .then(res => res.json())
-.then(data => displayCountries(data));
+.then(data => displayCountries(data))
 
+const displayCountries = countries => {
+    const countriesDiv = document.getElementById('countryList')
+    for (let i = 0; i < countries.length; i++) {
+        const country = countries[i]; 
+        const div = document.createElement('div')
+       div.className = 'country';
 
-const displayCountries = countries  =>{
-    console.log(countries)
-    const ul = document.getElementById('list');
+        // const h3 = document.createElement('h3')
+        // h3.innerText = country.name.common;
+        // const p = document.createElement('p')
+        // p.innerText = country.capital;
+        //  div.appendChild(h3)
+        //  div.appendChild(p)
 
-    for (let i = 0; i < countries.length -1; i++) {
-        const country = countries[i];
-        console.log(country)
-
-        const li = document.createElement('li');
-        li.innerText = country.name.common;
-        ul.appendChild(li);
+       const countryInfo = `
+       <h3 class = 'country-name'>${ country.name.common}</h3>
+       <p>${country.capital}</p>
+       `
+         div.innerHTML = countryInfo;
+         countriesDiv.appendChild(div)
+        
     }
 }
